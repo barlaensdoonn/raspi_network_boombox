8f7de76aeea8739ce69be6381a3d6d6c2bc30fbf
Unknown <brandon.aleson@gmail.com>
2017-12-29 20:02:11 -0800

    add udp broadcast example

--- send.py
+++ send.py
@@ -10,7 +10,7 @@ def encode_msg(track, volume):
     return '{}/{}\r\n'.format(track, volume).encode()
 
 
-def context_client(hostport, msg):
+def context_tcp_client(hostport, msg):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
         client.connect(hostport)
         client.sendall(msg)
@@ -18,10 +18,18 @@ def context_client(hostport, msg):
     return msg.decode().strip()
 
 
+def context_udp_broadcast(msg):
+    hostport = ('<broadcast>', 9999)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        return sock.sendto(msg, hostport)  # returns number of bytes sent
+
+
 if __name__ == '__main__':
     hostport = ('swnd.local', 9999)
     track = 'monteverdi'
     volume = '1.0'
 
-    msg = context_client(hostport, encode_msg(track, volume))
+    msg = context_tcp_client(hostport, encode_msg(track, volume))
     print('sent: {}'.format(msg))
